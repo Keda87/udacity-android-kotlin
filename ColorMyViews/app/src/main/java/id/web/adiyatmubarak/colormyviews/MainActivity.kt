192d96590c47c1eebd9a5855d95e952c5a57b6f1
Keda87 <adiyatmubarak@gmail.com>
2020-04-02 21:43:40 +0700

    Section 2: add boxes click listeners

--- ColorMyViews/app/src/main/java/id/web/adiyatmubarak/colormyviews/MainActivity.kt
+++ ColorMyViews/app/src/main/java/id/web/adiyatmubarak/colormyviews/MainActivity.kt
@@ -1,12 +1,44 @@
 package id.web.adiyatmubarak.colormyviews
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setListeners()
+    }
+
+    private fun setListeners() {
+        val clickableViews: List<View> = listOf(
+            box_one_text,
+            box_two_text,
+            box_three_text,
+            box_four_text,
+            box_five_text,
+            constraint_layout
+        )
+
+        clickableViews.forEach { it ->
+            it.setOnClickListener {
+                makeColored(it)
+            }
+        }
+    }
+
+    private fun makeColored(view: View) {
+        when(view.id) {
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.RED)
+            R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_blue_bright)
+            R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_orange_dark)
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
     }
 }
